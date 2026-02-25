62633f4043817dc7980b623becc1dbf9a299f79c
happyhzr <insisthzr@163.com>
2026-02-25 21:17:34 +0800

    add home & create page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     const { isSignedIn } = useAuth()
     return (
         <div className="navbar bg-base-300">
-            <div className="max-w-5xl max-auto w-full px-4 flex justify-between items-center">
+            <div className="max-w-5xl mx-auto w-full px-4 flex justify-between items-center">
                 <div className="flex-1">
                     <Link to="/" className="btn btn-ghost gap-2">
                         <ShoppingBagIcon className="size-5 text-primary" />
@@ -18,7 +18,7 @@ function Navbar() {
                     <ThemeSelector />
                     {isSignedIn ? (
                         <div>
-                            <Link to="/craete" className="btn btn-primary btn-sm gap-1">
+                            <Link to="/create" className="btn btn-primary btn-sm gap-1">
                                 <PlusIcon className="size-4" />
                                 <span className="hidden sm:inline">New Product</span>
                             </Link>
